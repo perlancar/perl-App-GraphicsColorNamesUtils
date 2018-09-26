6ed7586bce59f9e660286feac81de1ff9ae0bfc2
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-26 19:37:53 +0700

    Add utility: show-color-swatch; simplify CLIs (no config, no env)

--- lib/App/GraphicsColorNamesUtils.pm
+++ lib/App/GraphicsColorNamesUtils.pm
@@ -57,6 +57,48 @@ sub list_color_names {
     [200, "OK", \@rows, $resmeta];
 }
 
+$SPEC{show_color_swatch} = {
+    v => 1.1,
+    summary => 'List all color names from a Graphics::ColorNames scheme as a color swatch',
+    args => {
+        scheme => {
+            schema => 'perl::modname*',
+            req => 1,
+            pos => 0,
+        },
+        width => {
+            schema => 'posint*',
+            default => 80,
+            cmdline_aliases => {w=>{}},
+        },
+    },
+};
+sub show_color_swatch {
+    require Color::ANSI::Util;
+    require Color::RGB::Util;
+    require String::Pad;
+
+    my %args = @_;
+    my $width = $args{width} // 80;
+
+    my $res = list_color_names(scheme => $args{scheme}, detail=>1);
+    return $res unless $res->[0] == 200;
+
+    my $reset = Color::ANSI::Util::ansi_reset();
+    for my $row (@{ $res->[2] }) {
+        my $empty_bar = " " x $width;
+        my $text_bar  = String::Pad::pad("$row->{name} ($row->{rgb})", $width, "center", " ", 1);
+        my $bar = join(
+            "",
+            Color::ANSI::Util::ansibg($row->{rgb}), $empty_bar, $reset, "\n",
+            Color::ANSI::Util::ansibg($row->{rgb}), Color::ANSI::Util::ansifg(Color::RGB::Util::rgb_is_dark($row->{rgb}) ? "ffffff" : "000000"), $text_bar, $reset, "\n",
+            Color::ANSI::Util::ansibg($row->{rgb}), $empty_bar, $reset, "\n",
+            $empty_bar, "\n",
+        );
+        print $bar;
+    }
+    [200];
+}
 
 1;
 #ABSTRACT: Utilities related to Graphics::ColorNames
