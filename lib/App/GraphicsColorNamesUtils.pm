18794141ed384d09fde44e54ddb7ba87a123ac1a
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-26 16:22:32 +0700

    Initial commit

---
+++ lib/App/GraphicsColorNamesUtils.pm
@@ -0,0 +1,72 @@
+package App::GraphicsColorNamesUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{list_color_schemes} = {
+    v => 1.1,
+    summary => 'List all installed Graphics::ColorNames schemes',
+};
+sub list_color_schemes {
+    require Graphics::ColorNames;
+    my %args = @_;
+    [200, "OK", [Graphics::ColorNames::all_schemes]];
+}
+
+$SPEC{list_color_names} = {
+    v => 1.1,
+    summary => 'List all color names from a Graphics::ColorNames scheme',
+    args => {
+        scheme => {
+            schema => 'perl::modname*',
+            req => 1,
+            pos => 0,
+        },
+        detail => {
+            schema => 'true*',
+            cmdline_aliases => {l=>{}},
+        },
+    },
+};
+sub list_color_names {
+    require Graphics::ColorNames;
+
+    my %args = @_;
+
+    tie %colors, 'Graphics::ColorNames', 'Metallic';
+
+    my @rows;
+    my $resmeta = {};
+    for (sort keys %colors) {
+        push @rows, {name=>$_, rgb=>$colors{$_}};
+    }
+
+    if ($args{detail}) {
+        $resmeta->{'table.fields'} = [qw/name rgb/];
+    } else {
+        @rows = map {$_->{name}} @rows;
+    }
+
+    [200, "OK", \@rows, $resmeta];
+}
+
+
+1;
+#ABSTRACT: Utilities related to Graphics::ColorNames
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
